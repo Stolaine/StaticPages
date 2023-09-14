392cea9b7075fcf29ca3af3e06771e16a8c38292
Sheshank Kumar <sheshank.mukul@rameninc.com>
2023-09-14 13:15:05 +0530

    Convert imperative way to declarative way using React

--- index.js
+++ index.js
@@ -90,7 +90,12 @@
 //   document.getElementById("root")
 // );
 
-const h1 = document.createElement("h1");
-h1.textContent = "This is an imperative way to program";
-h1.className = "header";
-document.getElementById("root").append(h1);
+// const h1 = document.createElement("h1");
+// h1.textContent = "This is an imperative way to program";
+// h1.className = "header";
+// document.getElementById("root").append(h1);
+
+ReactDOM.render(
+  <h1 className="header">This is a declarative way to program</h1>,
+  document.getElementById("root")
+);
