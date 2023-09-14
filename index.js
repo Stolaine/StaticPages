2bfc3280265fad51fd3ed6436d70aefa7896b517
Sheshank Kumar <sheshank.mukul@rameninc.com>
2023-09-14 13:47:43 +0530

    Showing the issue with append method on root element

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-/* 
-Challenge: 
+/**
+Challenge: find out what happens if we try to append JSX
+to our div#root using .append() instead of ReactDOM
 
-Create a navbar in JSX:
-    - Use the semantic `nav` element as the parent wrapper
-    - Have an h1 element with the brand name of your "website"
-    - Insert an unordered list for the other nav elements
-        - Inside the `ul`, have three `li`s for "Pricing",
-        "About", and "Contact"
-    - Don't worry about styling yet - it'll just be plain-looking HTML for now
-*/
+1. Create a sample page in JSX (≥ 4 elements) and save them in a variable
+2. Select the div with the ID of "root" and use `.append()` to append
+   your JSX
+3. See if you can guess what will show up in the browser before running
+   the code
+4. See if you can explain what actually shows up in the browser
+ */
 
 const navbar = (
   <nav>
@@ -21,8 +21,10 @@ const navbar = (
   </nav>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(navbar);
+
+document.getElementById("root").append(JSON.stringify(navbar));
 
 // ReactDOM.render(navbar, document.getElementById("root"));
 // ReactDOM.createRoot(document.getElementById("root")).render(navbar);
