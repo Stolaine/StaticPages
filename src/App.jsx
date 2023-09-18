b8fbca498a22fdba4cdc6d54d811c4377580ef08
Sheshank Kumar <sheshank.mukul@rameninc.com>
2023-09-18 14:52:46 +0530

    Add data to show in custom component

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,23 @@ import "./App.css";
 function App() {
   return (
     <div>
-      <img src={reactLogo} width="50px" />
+      <header>
+        <nav>
+          <img src={reactLogo} width="50px" />
+        </nav>
+      </header>
       <h1>Fun facts about react</h1>
-      <ul>
+      <ol>
         <li>Was first released in 2013</li>
         <li>Was originally create by Jordan Walke</li>
         <li>Has well over 100K stars on Github</li>
         <li>Is maintained by Facebook</li>
         <li>Powers thousands of enterprise apps, including mobile apps</li>
-      </ul>
+      </ol>
+      <footer>
+        {" "}
+        <small>© 2023 Stolaine development. All rights reserved.</small>
+      </footer>
     </div>
   );
 }
