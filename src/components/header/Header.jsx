b2a20b50d925e12810c7c17f958879f1ed24e219
Sheshank Kumar <sheshank.mukul@rameninc.com>
2023-09-18 15:14:10 +0530

    Add styling using classes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,8 +2,13 @@ import reactLogo from "./../../assets/react-logo.png";
 function Header() {
   return (
     <header>
-      <nav>
-        <img src={reactLogo} width="50px" />
+      <nav className="nav">
+        <img className="nav-logo" src={reactLogo} />
+        <ul className="nav-items">
+          <li>Pricing</li>
+          <li>About</li>
+          <li>Contact</li>
+        </ul>
       </nav>
     </header>
   );
